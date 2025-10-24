b22ba06b059c23a2376767c0ce395fe0e2a7caf5
heitz45 <heitertluke@gmail.com>
2025-10-24 12:19:20 -0500

    Update index.js

--- api/jup/index.js
+++ api/jup/index.js
@@ -1,8 +1,5 @@
-export const config = { runtime: 'edge' };
-
-export default async function handler() {
-  return new Response(
-    JSON.stringify({ ok: true, service: 'jup-proxy' }),
-    { status: 200, headers: { 'content-type': 'application/json' } }
-  );
+export default async function handler(req, res) {
+  res.setHeader('content-type', 'application/json');
+  res.setHeader('access-control-allow-origin', '*');
+  return res.status(200).send(JSON.stringify({ ok: true, service: 'jup-proxy' }));
 }
